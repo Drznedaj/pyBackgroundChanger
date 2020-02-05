6d71718e06279849dd4a8056aa87662ad98417ca
nzaric-invn <nzaric@invensense.com>
2020-02-05 17:46:02 +0100

    Start implementing sync with gdrive

--- Favourites/favs.py
+++ Favourites/favs.py
@@ -1,27 +1,28 @@
 from PIL import Image
-import ctypes,os
+import ctypes
+import os
 
 i = 0
 
-with open('num.txt','r') as inf:
-	for chr in inf:
-		if chr >= '0' and chr <= '9':
-			i=int(chr)
-			print(i)
+with open('num.txt', 'r') as inf:
+    for chr in inf:
+        if chr >= '0' and chr <= '9':
+            i = int(chr)
+            print(i)
 
-i+=1
+i += 1
 
-with open('num.txt','w') as outf:
-	outf.write(str(i))
+with open('num.txt', 'w') as outf:
+    outf.write(str(i))
 
-pic_ = 'pyBackgroundChanger\\Pic_of_the_day.png'
-pic_path = os.path.join(os.path.join(os.environ["USERPROFILE"], "Desktop"), pic_)
+pic_ = 'picOday\\pyBackgroundChanger\\Pic_of_the_day.png'
+pic_path = os.path.join(os.path.split(os.path.dirname(os.path.abspath(__file__)))[0], pic_)
 exp_path = os.path.join(os.path.join(os.environ["USERPROFILE"], "Desktop"), 'Explanation.txt')
 
 im = Image.open(pic_path)
-im.save(str(i)+'.png','png')
+im.save(str(i)+'.png', 'png')
 
-with open(exp_path,'r') as inf:
-	with open(str(i)+'_explanation.txt','w') as outf:
-		for line in inf:
-			outf.write(line)
+with open(exp_path, 'r') as inf:
+    with open(str(i)+'_explanation.txt', 'w') as outf:
+        for line in inf:
+            outf.write(line)
--- bckgd_auto.py
+++ bckgd_auto.py
@@ -4,19 +4,13 @@ import urllib.request
 from html.parser import HTMLParser
 
 u = urllib.request.urlopen('https://apod.nasa.gov/apod/astropix.html')  # opens the nasa pic of the day page
-
-f = open("Url_to_parse.txt", "w")  # saving the html to text and decoding
-f.write(u.read().decode("utf-8", errors='ignore'))
-f.close()
+decoded_str = u.read().decode(encoding='utf-8').split('\n')
 
 picture = 'https://apod.nasa.gov/apod/'  # prefix to the url of the picture
 
 outs = ""
 
-explntn = 'Explanation.txt'
-pic_ = 'background changer\\Pic_of_the_day.png'
-explntn_path = os.path.join(os.path.join(os.environ["USERPROFILE"], "Desktop"), explntn)
-pic_path = os.path.join(os.path.join(os.environ["USERPROFILE"], "Desktop"), pic_)
+explntn_path = os.path.join(os.path.join(os.environ["USERPROFILE"], "Desktop"), 'Explanation.txt')
 
 
 class MyHTMLParser(HTMLParser):
@@ -26,24 +20,21 @@ class MyHTMLParser(HTMLParser):
 
 
 parser = MyHTMLParser()
+for line in decoded_str:  # and feeds lines to a html parser to get the explanation
+    parser.feed(line)  # of the picture
+    if (picture != 'https://apod.nasa.gov/apod/'):
+        continue
+    elif '.png' in line or '.jpg' in line:
+        x = line.split('\"')[1]  # this just gets the url without the html tags
+        picture += x
+        print(picture)
+    elif 'youtube' in line:
+        yt_link = 'Not Found'
+        for l in line.split(' '):
+            if 'youtube' in l:
+                yt_link = l.split('\"')[1]
+        print('Today there is a video on: {}'.format(yt_link))
 
-with open('Url_to_parse.txt', 'r') as inF:  # finding the rest of the url of the picture in the file
-    for line in inF:  # and feeds lines to a html parser to get the explanation
-        parser.feed(line)  # of the picture
-        if (picture != 'https://apod.nasa.gov/apod/'):
-            continue
-        elif '.png' in line or '.jpg' in line:
-            x = line.split('\"')[1]  # this just gets the url without the html tags
-            picture += x
-            print(picture)
-        elif 'youtube' in line:
-            yt_link = 'Not Found'
-            for l in line.split(' '):
-                if 'youtube' in l:
-                    yt_link = l.split('\"')[1]
-            print('Today there is a video on: {}'.format(yt_link))
-
-print(outs)
 
 # saves the explanation of the picture to a file on the desktop
 with open(explntn_path, 'w') as outF:
