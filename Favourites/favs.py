566657dd240f7f02c42d3c44e8eaeb0e33b073c9
Drznedaj <cvajblumen@gmail.com>
2021-11-17 22:01:09 +0100

    More f strings, a little refactor and pipenv

--- Favourites/favs.py
+++ Favourites/favs.py
@@ -1,25 +1,29 @@
 from PIL import Image
 import os
 
-i = 0
+pic_number = 0
+maximum_pic_num = 0
 dir_path = os.path.dirname(os.path.abspath(__file__))
 base_path = os.path.split(dir_path)[0]
 
-for top, _, files in os.walk(dir_path):
+for _, _, files in os.walk(dir_path):
     for f in files:
         if '.png' in f or '.jpg' in f:
-            i = int(f.split('.')[0])
+            pic_number = int(f.split('.')[0])
+            if pic_number > maximum_pic_num:
+                maximum_pic_num = pic_number
 
-print('Saving favourite picture number: {}'.format(i))
+maximum_pic_num += 1
+print(f'Saving favourite picture number: {maximum_pic_num}')
 
 pic_ = 'Pic_of_the_day.png'
 pic_path = os.path.join(base_path, pic_)
 exp_path = os.path.join(os.path.join(os.environ["USERPROFILE"], "Desktop"), 'Explanation.txt')
 
 im = Image.open(pic_path)
-im.save(str(i)+'.png', 'png')
+im.save(f'{maximum_pic_num}.png', 'png')
 
 with open(exp_path, 'r') as inf:
-    with open(str(i)+'_explanation.txt', 'w') as outf:
+    with open(f'{maximum_pic_num}_explanation.txt', 'w') as outf:
         for line in inf:
             outf.write(line)
