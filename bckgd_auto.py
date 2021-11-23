5152978bbbbc36eff82ae18fc87f55e6619a294e
Drznedaj <cvajblumen@gmail.com>
2021-11-23 23:24:11 +0100

    Add beautiful soup, use it to get explanation text

--- bckgd_auto.py
+++ bckgd_auto.py
@@ -2,53 +2,41 @@ import os
 import urllib.request as ur
 from urllib.error import HTTPError
 from html.parser import HTMLParser
+from bs4 import BeautifulSoup
 import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 u = ur.urlopen('https://apod.nasa.gov/apod/astropix.html')  # opens the nasa pic of the day page
-decoded_str = u.read().decode(encoding='utf-8').split('\n')
+soup = BeautifulSoup(u.read().decode(encoding='utf-8'), 'html.parser')
 
-picture = 'https://apod.nasa.gov/apod/'  # prefix to the url of the picture
+explanation = soup.find_all('p')[2].get_text()
 
-outs = ""
+picture_suffix = ''
+
+for link in soup.find_all('a'):
+    lnk = link.get('href')
+    if 'image' in lnk:
+        picture_suffix = lnk
+        break
+
+picture_prefix = 'https://apod.nasa.gov/apod/'  # prefix to the url of the picture
+picture_url = f'{picture_prefix}{picture_suffix}'
+print(picture_url)
 
 explntn_path = os.path.join(os.path.join(os.environ["USERPROFILE"], "Desktop"), 'Explanation.txt')
 
-
-class MyHTMLParser(HTMLParser):
-    def handle_data(self, data):
-        global outs
-        outs += data + '\n'
-
-
-parser = MyHTMLParser()
-for line in decoded_str:    # feed lines to a html parser to get the explanation
-    parser.feed(line)       # of the picture
-    if (picture != 'https://apod.nasa.gov/apod/'):
-        continue
-    elif '.png' in line or '.jpg' in line:
-        x = line.split('\"')[1]  # this just gets the url without the html tags
-        picture += x
-        print(picture)
-    elif 'youtube' in line:
-        yt_link = 'Not Found'
-        for l in line.split(' '):
-            if 'youtube' in l:
-                yt_link = l.split('\"')[1]
-        print(f'Today there is a video on: {yt_link}')
-
 # saves the explanation of the picture to a file on the desktop
 with open(explntn_path, 'w') as outF:
-    outF.write(outs[51:-326])
+    outF.write(explanation)
 
 # this is a url of a picture of lower quality if we get an error
 # downloading the first one we can try this one
-pic1 = picture[:-4]+'1024.jpg'
+pic1 = picture_url[:-4]+'1024.jpg'
 
 try:
     # this part gets the actual picture and saves it to a file
-    ur.urlretrieve(picture, 'Pic_of_the_day.png')
+    ur.urlretrieve(picture_url, 'Pic_of_the_day.png')
 
 except HTTPError as e:
     print(f'There was an error: {e.reason} trying to download picture of a lower quality...')
