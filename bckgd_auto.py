aefca9f377aca4a5194d2934708f128af54b95b6
Drznedaj <cvajblumen@gmail.com>
2022-05-02 17:02:32 +0200

    Refactor background changer with functions

--- bckgd_auto.py
+++ bckgd_auto.py
@@ -1,43 +1,53 @@
 import os
 import urllib.request as ur
 from urllib.error import HTTPError
-from html.parser import HTMLParser
 from bs4 import BeautifulSoup
 import ssl
 
-ssl._create_default_https_context = ssl._create_unverified_context
 
-u = ur.urlopen('https://apod.nasa.gov/apod/astropix.html')  # opens the nasa pic of the day page
-soup = BeautifulSoup(u.read().decode(encoding='utf-8'), 'html.parser')
+def save_explanation(soup: BeautifulSoup, explntn_path: str):
+    explanation = soup.find_all('p')[2].get_text()
 
-explanation = soup.find_all('p')[2].get_text()
+    # saves the explanation of the picture to a file on the desktop
+    with open(explntn_path, 'w') as outF:
+        outF.write(explanation)
 
-picture_suffix = ''
 
-for link in soup.find_all('a'):
-    lnk = link.get('href')
-    if 'image' in lnk:
-        picture_suffix = lnk
-        break
+def save_pictrure(soup, picture_prefix):
+    picture_suffix = ''
 
-picture_prefix = 'https://apod.nasa.gov/apod/'  # prefix to the url of the picture
-picture_url = f'{picture_prefix}{picture_suffix}'
-print(picture_url)
+    for link in soup.find_all('a'):
+        lnk = link.get('href')
+        if 'image' in lnk:
+            picture_suffix = lnk
+            break
 
-explntn_path = os.path.join(os.path.join(os.environ["USERPROFILE"], "Desktop"), 'Explanation.txt')
+    picture_url = f'{picture_prefix}{picture_suffix}'
+    print(picture_url)
 
-# saves the explanation of the picture to a file on the desktop
-with open(explntn_path, 'w') as outF:
-    outF.write(explanation)
+    # this is a url of a picture of lower quality if we get an error
+    # downloading the first one we can try this one
+    pic1 = picture_url[:-4]+'1024.jpg'
 
-# this is a url of a picture of lower quality if we get an error
-# downloading the first one we can try this one
-pic1 = picture_url[:-4]+'1024.jpg'
+    try:
+        # this part gets the actual picture and saves it to a file
+        ur.urlretrieve(picture_url, 'Pic_of_the_day.png')
 
-try:
-    # this part gets the actual picture and saves it to a file
-    ur.urlretrieve(picture_url, 'Pic_of_the_day.png')
+    except HTTPError as e:
+        print(f'There was an error: {e.reason} trying to download picture of a lower quality...')
+        ur.urlretrieve(pic1, 'Pic_of_the_day.png')
 
-except HTTPError as e:
-    print(f'There was an error: {e.reason} trying to download picture of a lower quality...')
-    ur.urlretrieve(pic1, 'Pic_of_the_day.png')
+
+if __name__ == '__main__':
+
+    picture_prefix = 'https://apod.nasa.gov/apod/'  # prefix to the url of the picture
+    explntn_path = os.path.join(os.path.join(os.environ["USERPROFILE"], "Desktop"), 'Explanation.txt')
+
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+    u = ur.urlopen(f'{picture_prefix}/astropix.html')  # opens the nasa pic of the day page
+    soup = BeautifulSoup(u.read().decode(encoding='utf-8'), 'html.parser')
+
+    save_explanation(soup, explntn_path)
+
+    save_pictrure(soup, picture_prefix)
