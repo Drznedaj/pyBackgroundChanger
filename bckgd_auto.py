08f6c636d87bd238c848e7a444555fb1983b5cae
Drznedaj <cvajblumen@gmail.com>
2022-05-03 14:32:46 +0200

    Add loading animation

--- bckgd_auto.py
+++ bckgd_auto.py
@@ -1,4 +1,8 @@
 import os
+import itertools
+import threading
+import time
+import sys
 import urllib.request as ur
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
@@ -13,8 +17,7 @@ def save_explanation(soup: BeautifulSoup, explntn_path: str):
         outF.write(explanation)
 
 
-def save_pictrure(soup, picture_prefix):
-    picture_suffix = ''
+def save_pictrure(soup: BeautifulSoup, picture_prefix: str):
 
     for link in soup.find_all('a'):
         lnk = link.get('href')
@@ -22,8 +25,10 @@ def save_pictrure(soup, picture_prefix):
             picture_suffix = lnk
             break
 
+    picture_suffix = picture_suffix if picture_suffix else ''
+
     picture_url = f'{picture_prefix}{picture_suffix}'
-    print(picture_url)
+    print(f'\n{picture_url}')
 
     # this is a url of a picture of lower quality if we get an error
     # downloading the first one we can try this one
@@ -38,7 +43,23 @@ def save_pictrure(soup, picture_prefix):
         ur.urlretrieve(pic1, 'Pic_of_the_day.png')
 
 
+done = False
+
+
+def animate():
+    for c in itertools.cycle(["⢿", "⣻", "⣽", "⣾", "⣷", "⣯", "⣟", "⡿"]):
+        if done:
+            break
+        sys.stdout.write(f'\rloading {c}')
+        sys.stdout.flush()
+        time.sleep(0.1)
+    sys.stdout.write('\rDownload complete!     ')
+
+
 if __name__ == '__main__':
+
+    t = threading.Thread(target=animate)
+    t.start()
 
     picture_prefix = 'https://apod.nasa.gov/apod/'  # prefix to the url of the picture
     explntn_path = os.path.join(os.path.join(os.environ["USERPROFILE"], "Desktop"), 'Explanation.txt')
@@ -51,3 +72,5 @@ if __name__ == '__main__':
     save_explanation(soup, explntn_path)
 
     save_pictrure(soup, picture_prefix)
+
+    done = True
